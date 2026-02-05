01a1e3af39b85904b1b1f16c8c185ec4d6d4cb8b
Tobias Olveira <tolveira@d_hipolito.msi>
2026-02-05 14:25:01 -0300

    quinto commit

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -4,99 +4,18 @@ const VALID_ROLES = ["BOE Patrulla", "BOE Admin"];
 
 export const loginUser = async (credentials) => {
   try {
-    const response = {
-      data: {
-        success: true,
-        user: {
-          dni: "21923152",
-          name: "CHISNERMAN, MAXIMILIANO CESAR",
-          oficina: "Subsecretaría de Innovación",
-          funcion: "Programador",
-          imagen: null,
-          roles: [
-            "Administrador",
-            "Administrador Incidentes",
-            "Agendas",
-            "ARSI",
-            "ARSI Cuenta Corriente",
-            "ARSI Dirección",
-            "ARSI Revisión",
-            "BOE Admin",
-            "BOE Incidentes",
-            "Cartola Permiso",
-            "Consulta Ciudadana",
-            "Defensa Del Consumidor",
-            "Delegaciones",
-            "DGE",
-            "Digitalización",
-            "Director de Obras",
-            "Director General",
-            "Exenciones",
-            "Incidentes",
-            "Legales",
-            "Licencias",
-            "Licencias Dashboard",
-            "Licencias Superuser",
-            "Municipal",
-            "Obras Particulares",
-            "Planeamiento Urbano",
-            "Recursos Humanos",
-            "RRHH-Loys",
-            "Servicios Asistencia",
-            "Solucionador",
-            "Supervisor Digitalización",
-            "Tablero de Control",
-            "Validador Planeamiento",
-            "Visualizador",
-          ],
-          qr: "https://tesi.sanisidro.gob.ar/confirmacionCredencial/eyJpdiI6IktBQWdtWkkrQnJjZzlNTXFhQlJJT2c9PSIsInZhbHVlIjoidktUN1cyMlNBVjU2SDRTZjFrV29EQT09IiwibWFjIjoiZDc0NmRmMDAzYTJhNjJmZmMzYzg0YWRkZTcxYzU1YTNmZTI0N2ViY2NlMDg1ZTc2OWU5MTMwY2IxZjI5MzFhOSIsInRhZyI6IiJ9",
-          id: 1331,
-        },
+    const response = await axios.post(
+      "/tesi-api/v1.0/chequeoCredencialesFull",
+      {
+        cuit: credentials.cuit,
+        password: credentials.password,
       },
-      status: 200,
-      statusText: "OK",
-      headers: {
-        "access-control-allow-credentials": "true, true",
-        "access-control-allow-headers":
-          "Content-Type, Authorization, X-Requested-With",
-        "access-control-allow-methods": "GET, POST, OPTIONS",
-        "access-control-allow-origin": "http://localhost:5173",
-        "cache-control": "no-cache, private",
-        connection: "close",
-        "content-encoding": "gzip",
-        "content-type": "application/json",
-        date: "Thu, 05 Feb 2026 16:38:32 GMT",
-        server: "nginx/1.20.1",
-        "transfer-encoding": "chunked",
-        vary: "Accept-Encoding, Origin",
-        "x-powered-by": "PHP/8.1.29",
-      },
-      config: {
-        transitional: {
-          silentJSONParsing: true,
-          forcedJSONParsing: true,
-          clarifyTimeoutError: false,
-        },
-        adapter: ["xhr", "http", "fetch"],
-        transformRequest: [null],
-        transformResponse: [null],
-        timeout: 0,
-        xsrfCookieName: "XSRF-TOKEN",
-        xsrfHeaderName: "X-XSRF-TOKEN",
-        maxContentLength: -1,
-        maxBodyLength: -1,
-        env: {},
+      {
         headers: {
-          Accept: "application/json, text/plain, */*",
           "Content-Type": "application/json",
         },
-        method: "post",
-        url: "/tesi-api/v1.0/chequeoCredencialesFull",
-        data: '{"cuit":"20219231521","password":"123"}',
-        allowAbsoluteUrls: true,
       },
-      request: {},
-    };
+    );
     console.log(response);
     if (response.data?.success && response.data?.user) {
       const user = response.data.user;
