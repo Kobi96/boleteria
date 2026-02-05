338e6c969313a0e08adc60b46def9b74fbaed0ad
Tobias Olveira <tolveira@d_hipolito.msi>
2026-02-05 15:38:48 -0300

    octavo commit

--- src/components/QRScanner.jsx
+++ src/components/QRScanner.jsx
@@ -62,7 +62,7 @@ const QRScanner = () => {
       const encodedQR = encodeURIComponent(qrData);
       console.log(encodedQR);
       const response = await axios.get(
-        `/qr-api/servicios/lectura/${encodedQR}`,
+        `https://staging-dev.sanisidro.gob.ar/api/qr/servicios/lectura/${encodedQR}`,
       );
 
       console.log("Respuesta de la API:", response.data);
